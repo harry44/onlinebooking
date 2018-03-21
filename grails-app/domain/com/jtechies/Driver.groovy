8a0e07e36b9cd167908769b3beb0c030eedfc242
harry44 <harnarayan44@gmail.com>
2018-03-21 23:23:42 +0530

    Driver Domain

---
+++ grails-app/domain/com/jtechies/Driver.groovy
@@ -0,0 +1,17 @@
+package com.jtechies
+
+class Driver {
+	Integer id
+	String driverName
+	String licenceId
+	byte[] driverPhoto
+	String contactNumber
+	String emailId
+	Date dateOfBirth
+	///////////////// Default fields //////////////
+	Date dateCreated
+	Date lastUpdated
+
+	static constraints = {
+	}
+}
